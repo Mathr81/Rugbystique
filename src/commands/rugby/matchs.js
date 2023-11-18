6c0cf134317a099483cde8fb38ec40302bef1d45
Mathr81 <112965202+Mathr81@users.noreply.github.com>
2023-11-18 15:16:35 +0000

    Added /matchs command 	deleted:    src/commands/rugby/bet.js 	new file:   src/commands/rugby/matchs.js 	modified:   src/package-lock.json 	modified:   src/package.json

---
+++ src/commands/rugby/matchs.js
@@ -0,0 +1,63 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("matchs")
+    .setDescription("Donne les matchs d'une certaine date")
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(null)
+    .addStringOption(opt => opt.setName("date").setDescription("La date des matchs au format : 2023-11-18").setRequired(false)),
+
+    async run(interaction) {
+
+        function getDateString() {
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1; // Add 1 because the getMonth() function returns a zero-based value
+            const day = date.getDate();
+           
+            return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+           }
+
+        var date = interaction.options.getString("date");
+        if(!date) {
+            date = getDateString()
+        }
+
+        var config = {
+            method: 'get',
+            url: `https://v1.rugby.api-sports.io/games?league=16&season=2023&date=${date}`,
+            headers: {
+              'x-rapidapi-key': process.env.API_KEY,
+              'x-rapidapi-host': 'v1.rugby.api-sports.io'
+            }
+          };
+          
+          axios(config)
+          .then(async function (response) {
+            const matchData = response.data.response.map(match => {
+                const { home, away } = match.teams;
+                return {
+                  homeTeam: home.name,
+                  awayTeam: away.name,
+                  matchTime: match.time,
+                };
+              });
+              
+              const embed = {
+                color: 0x0099ff,
+                title: 'Matchs',
+                description: matchData.map(match => {
+                 return `**${match.homeTeam}** vs **${match.awayTeam}** à ${match.matchTime}`;
+                }).join('\n\n'),
+              };
+
+              await interaction.reply({ embeds: [embed] });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+};
