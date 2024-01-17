02fb81f5af35a4b9e9f1fc2c225e517e60fddc55
Mathr81 <matheobarthes@gmail.com>
2024-01-17 17:00:27 +0100

    Added a classement command 	new file:   src/commands/rugby/top14.html 	new file:   src/commands/rugby/top14.js 	modified:   src/package-lock.json 	modified:   src/package.json 	new file:   src/screenshot.png

---
+++ src/commands/rugby/top14.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('discord.js');
+const puppeteer = require('puppeteer');
+const path = require('path')
+const filePath = path.resolve(__dirname, 'top14.html');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("top14")
+    .setDescription("Get top14 classement")
+    .setDMPermission(true)
+    .setDefaultMemberPermissions(null),
+
+    async run(interaction) {
+        await interaction.deferReply()
+        // Lancez un navigateur Chrome
+        const browser = await puppeteer.launch({ headless: "new" });
+
+        // Ouvrez une nouvelle page
+        const page = await browser.newPage();
+        await page.setViewport({ width: 800, height: 800, deviceScaleFactor: 15 })
+
+        // Accédez à la page index.html
+        await page.goto(filePath, {waitUntil: 'networkidle2'});
+
+        const element = await page.$('#wg-api-rugby-standings');
+        await element.screenshot({path: 'screenshot.png', clip: {x: 0, y: 0, width: 800, height: 800}});
+
+        // Prenez une capture d'écran de la page et enregistrez-la sous le nom "screenshot.png"
+        //await page.screenshot({path: 'screenshot.png'});
+
+        // Fermez le navigateur
+        await browser.close();
+
+        await interaction.followUp({files: ['screenshot.png']})
+        //fs.unlinkSync('screenshot.png');
+    }
+};
